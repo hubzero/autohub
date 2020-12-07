36a9da0bab6b5aa62aaa3631711ecc0a3911e455
Jeff Kayser <jeff.kayser@gmail.com>
2020-12-07 17:45:30 -0500

    Add vim to base image and mouse support for tmux

--- packer/scripts/custom.sh
+++ packer/scripts/custom.sh
@@ -1,5 +1,6 @@
 usermod -a -G apache vagrant
-yum -y install colordiff jq ncdu php56-php-pecl-xdebug tmux tree
+yum -y install colordiff jq ncdu php56-php-pecl-xdebug tmux tree vim
 git config --global color.ui auto
 mv -f ~/.gitconfig /etc/gitconfig
 echo export GREP_OPTIONS='--color=auto' >> /etc/bashrc
+echo 'set -g mouse-mode on' > /home/vagrant/.tmux.conf
