cf11efc7e0fefb46ddc2a94d33ce13c36e8dd6ad
Jeff Kayser <jeff.kayser@gmail.com>
2020-12-07 17:46:53 -0500

    Remove unneeded OpenVZ files

--- packer/scripts/vmtools.sh
+++ packer/scripts/vmtools.sh
@@ -1,7 +1,5 @@
 yum -y install bzip2 net-tools
 yum -y install gcc make perl
-rpm -i http://download.openvz.org/kernel/branches/rhel6-2.6.32/042stab142.1/vzkernel-devel-2.6.32-042stab142.1.x86_64.rpm
-rpm -i http://download.openvz.org/kernel/branches/rhel6-2.6.32/042stab142.1/vzkernel-headers-2.6.32-042stab142.1.x86_64.rpm
 VBOX_VERSION=$(cat /home/vagrant/.vbox_version)
 cd /tmp
 mount -o loop /home/vagrant/VBoxGuestAdditions_${VBOX_VERSION}.iso /mnt
